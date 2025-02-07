c41a7fdd05761f0ed094a81b377825dc1badf39a
Ilie Danila <ilie.danila@proton.me>
2025-02-07 20:04:21 +0200

    Added environment.ts and edited .editorconfig

---
+++ apps/knowledge-graph/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDucPTxS82x-rnChqCnfVAlG-RcBK0sXEE",
+        authDomain: "knowledgegraph-72939.firebaseapp.com",
+        projectId: "knowledgegraph-72939",
+        storageBucket: "knowledgegraph-72939.firebasestorage.app",
+        messagingSenderId: "51304440744",
+        appId: "1:51304440744:web:7859f2b285bb33afd0339d",
+    },
+};
